5cc35cffe38ecd738b43d0a35d50a14e03f11a6b
Ashwin <161915771+Ashw-in2006@users.noreply.github.com>
2025-12-09 21:24:13 +0530

    starchange

--- star.java
+++ star.java
@@ -3,9 +3,11 @@ public class star {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int var = sc.nextInt();
-        for(int i = 0; i < var; i++){
-            for(int j = i; j <= var-1 ; j++){
-                System.out.print("* ");
+        
+        for(int i = 1; i <= var; i++){           //Row           //for(int i = 0; i < var; i++)
+            for(int j = 1; j <= i ; j++){       //Column        //for(int j = i; j <= var-1 ; j++)  for(int j = 0; j <= i ; j++)
+                
+                System.out.print(j+"");
             }System.out.println();
         }
         
