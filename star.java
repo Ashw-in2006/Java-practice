0d34e47548e98ee1148b34b63533ed07df29abe4
Ashwin <161915771+Ashw-in2006@users.noreply.github.com>
2025-12-10 21:59:13 +0530

    starto01

--- star.java
+++ star.java
@@ -3,12 +3,16 @@ public class star {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int var = sc.nextInt();
-        
+        int val = 1;
         for(int i = 1; i <= var; i++){           //Row           //for(int i = 0; i < var; i++)
+            val = i%2==0 ? 0 : 1;
             for(int j = 1; j <= i ; j++){       //Column        //for(int j = i; j <= var-1 ; j++)  for(int j = 0; j <= i ; j++)
                 
-                System.out.print(j+"");
-            }System.out.println();
+                System.out.print(val+" ");
+                
+                val = val!=0 ? 0 : 1;
+            }
+            System.out.println();
         }
         
     }
