8e9b618ed8e9f0c5f8f98a1426d8d89616a8d67a
Ashwin <161915771+Ashw-in2006@users.noreply.github.com>
2025-12-11 06:13:24 +0530

    startostat

--- star.java
+++ star.java
@@ -1,6 +1,39 @@
 import java.util.*;
 public class star {
-    public static void main(String[] args) {
+    void patternA(){
+        Scanner sc = new Scanner(System.in);
+        int var = sc.nextInt();
+        for(int i = 0; i < var; i++){           //Row           //for(int i = 0; i < var; i++)
+            for(int j = 0; j< var ; j++){       //Column        //for(int j = i; j <= var-1 ; j++)  for(int j = 0; j <= i ; j++)
+                System.out.print("*");
+            }
+            System.out.println();
+    }
+}
+//------------------------------------------------------------------------------------------------------//
+      void patternB(){
+        Scanner sc = new Scanner(System.in);
+        int var = sc.nextInt();
+        for(int i = 0; i < var; i++){           //Row           //for(int i = 0; i < var; i++)
+            for(int j = 0; j< var ; j++){       //Column        //for(int j = i; j <= var-1 ; j++)  for(int j = 0; j <= i ; j++)
+                System.out.print(j);
+            }
+            System.out.println();
+    }
+}
+//----------------------------------------------------------------------------------------------------------//
+    void patternC(){
+        Scanner sc = new Scanner(System.in);
+        int var = sc.nextInt();
+        for(int i = 1; i <= var; i++){           //Row           //for(int i = 0; i < var; i++)
+            for(int j = 1; j <= i ; j++){       //Column        //for(int j = i; j <= var-1 ; j++)  for(int j = 0; j <= i ; j++
+                System.out.print(j+" ");
+            }
+            System.out.println();
+    }
+}
+    //--------------------------------------------------------------------------//
+        void patternD(){
         Scanner sc = new Scanner(System.in);
         int var = sc.nextInt();
         int val = 1;
@@ -13,7 +46,31 @@ public class star {
                 val = val!=0 ? 0 : 1;
             }
             System.out.println();
+    }
+}
+//------------------------------------------------------------------------------------//
+        void patternE(){
+        Scanner sc = new Scanner(System.in);
+        int var = sc.nextInt();
+        int val = 1;
+        for(int i = 1; i <= var; i++){           //Row           //for(int i = 0; i < var; i++)
+            
+            for(int j = 1; j <= i ; j++){       //Column        //for(int j = i; j <= var-1 ; j++)  for(int j = 0; j <= i ; j++)
+                
+                System.out.print(val++);
+                System.out.print(" ");
+                
+            }
+            System.out.println();
+    }
+}
+//------------------------------------------------------------------------------------//
+    public static void main(String[] args) {
+        star ob= new star();
+        ob.patternA();
+
         }
         
     }
-}
+    
+
